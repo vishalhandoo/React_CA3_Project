98bc12b19e20e633c6df2ccde9ca1f1a0e679006
vishalhandoo <vishalhandoo067@gmail.com>
2023-04-17 16:55:34 +0530

    API Commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
-import Picker from 'emoji-picker-react';
-import './App.css';
- 
+import React, { useEffect, useState } from "react";
+import emojiData from "./emojiData.json";
+import "./index.css";
 function App() {
- 
-  const [inputStr, setInputStr] = useState('');
-  const [showPicker, setShowPicker] = useState(false);
- 
-  const onEmojiClick = (event, emojiObject) => {
-    setInputStr(prevInput => prevInput + emojiObject.emoji);
-    setShowPicker(false);
-  };
- 
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const newData = emojiData.filter((emoji) =>
+      emoji.title.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+    setData(newData);
+  }, [search]);
+
   return (
-    <div className="app">
-      <h3>Add Emoji Picker</h3>
-      <div className="picker-container">
-        <input
-          className="input-style"
-          value={inputStr}
-          onChange={e => setInputStr(e.target.value)} />
-        <img
-          className="emoji-icon"
-          src="https://icons.getbootstrap.com/assets/icons/emoji-smile.svg"
-          onClick={() => setShowPicker(val => !val)} />
-        {showPicker && <Picker
-          pickerStyle={{ width: '100%' }}
-          onEmojiClick={onEmojiClick} />}
-      </div>
+    <div>
+      <h1>Emoji Search</h1>
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      {data.map((emoji) => (
+        <h2 key={emoji.title}>
+          {emoji.title} {emoji.symbol}
+        </h2>
+      ))}
     </div>
   );
 }
+
 export default App;
